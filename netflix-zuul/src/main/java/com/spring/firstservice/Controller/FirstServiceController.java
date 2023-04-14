a0a61e7cd9fc3cfc80c081e4706152499afea7c7
js988174 <76925402+js988174@users.noreply.github.com>
2023-04-14 19:26:52 +0900

    Filter 적용

--- netflix-zuul/src/main/java/com/spring/firstservice/Controller/FirstServiceController.java
+++ netflix-zuul/src/main/java/com/spring/firstservice/Controller/FirstServiceController.java
@@ -1,15 +1,24 @@
 package com.spring.firstservice.Controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/first-service/")
+@Slf4j
 public class FirstServiceController {
 
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the first service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Hello first Service";
     }
 }
