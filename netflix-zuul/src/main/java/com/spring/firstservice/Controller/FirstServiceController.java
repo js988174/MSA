60f37587512137d5f6ea287db070f54381ab593c
js988174 <76925402+js988174@users.noreply.github.com>
2023-04-18 19:48:21 +0900

    Filter 수정

--- netflix-zuul/src/main/java/com/spring/firstservice/Controller/FirstServiceController.java
+++ netflix-zuul/src/main/java/com/spring/firstservice/Controller/FirstServiceController.java
@@ -1,15 +1,27 @@
 package com.spring.firstservice.Controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service/")
 @Slf4j
 public class FirstServiceController {
+
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
+
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,7 +35,8 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "message check";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format("message check on POST %s", env.getProperty("local.server.port"));
     }
 }
