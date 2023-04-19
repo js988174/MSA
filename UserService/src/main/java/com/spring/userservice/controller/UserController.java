4a0e07a667251ef2d498a00a1ea0b44eed89ecad
js988174 <76925402+js988174@users.noreply.github.com>
2023-04-19 17:10:31 +0900

    User Create 로직

--- UserService/src/main/java/com/spring/userservice/controller/UserController.java
+++ UserService/src/main/java/com/spring/userservice/controller/UserController.java
@@ -5,10 +5,13 @@ import com.spring.userservice.dto.UserDto;
 import com.spring.userservice.service.UserService;
 import com.spring.userservice.vo.Greeting;
 import com.spring.userservice.vo.RequestUser;
+import com.spring.userservice.vo.ResponseUser;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -39,13 +42,15 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public String createUser(@RequestBody RequestUser user) {
+    public ResponseEntity createUser(@RequestBody RequestUser user) {
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         UserDto userDto = mapper.map(user, UserDto.class);
         userService.createUser(userDto);
 
-        return "create User";
+        ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
     }
 }
---
+++ UserService/src/main/java/com/spring/userservice/vo/ResponseUser.java
@@ -0,0 +1,4 @@
+package com.spring.userservice.vo;
+
+public class ResponseUser {
+}
